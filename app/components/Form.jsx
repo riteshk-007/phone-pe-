69f738dd6aec2a43fb1d881b416c3348f0509293
Riteshk_007 <codeshorts007@gmail.com>
2024-05-11 14:36:51 +0530

    chore: Update dependencies and environment configuration

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,13 +1,67 @@
 "use client";
 import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import sha256 from "crypto-js/sha256";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 const Form = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const transactionid = "Tr-" + uuidv4().toString(36).slice(-6);
+
+    const payload = {
+      merchantId: process.env.NEXT_PUBLIC_MERCHANT_ID,
+      merchantTransactionId: transactionid,
+      merchantUserId: "MUID-" + uuidv4().toString(36).slice(-6),
+      amount: 10000,
+      redirectUrl: `http://localhost:3000/api/status/${transactionid}`,
+      redirectMode: "POST",
+      callbackUrl: `http://localhost:3000/api/status/${transactionid}`,
+      mobileNumber: data.mobile,
+      paymentInstrument: {
+        type: "PAY_PAGE",
+      },
+    };
+
+    const dataPayload = JSON.stringify(payload);
+    console.log(dataPayload);
+
+    const dataBase64 = Buffer.from(dataPayload).toString("base64");
+    console.log(dataBase64);
+
+    const fullURL =
+      dataBase64 + "/pg/v1/pay" + process.env.NEXT_PUBLIC_SALT_KEY;
+    const dataSha256 = sha256(fullURL);
+
+    const checksum = dataSha256 + "###" + process.env.NEXT_PUBLIC_SALT_INDEX;
+    console.log("c====", checksum);
+
+    const UAT_PAY_API_URL =
+      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+
+    const response = await axios.post(
+      UAT_PAY_API_URL,
+      {
+        request: dataBase64,
+      },
+      {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          "X-VERIFY": checksum,
+        },
+      }
+    );
+
+    const redirect = response.data.data.instrumentResponse.redirectInfo.url;
+    router.push(redirect);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full">
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_MERCHANT_ID: process.env.NEXT_PUBLIC_MERCHANT_ID,
+    NEXT_PUBLIC_SALT_KEY: process.env.NEXT_PUBLIC_SALT_KEY,
+    NEXT_PUBLIC_SALT_INDEX: process.env.NEXT_PUBLIC_SALT_INDEX,
+  },
+};
 
 export default nextConfig;
